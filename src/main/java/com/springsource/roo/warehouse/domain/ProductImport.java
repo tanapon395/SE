5840ee80e76b3775b4d4de68a55789e614ae1c60
tanapon395@gmail.com <tanapon395@gmail.com>
2016-12-26 21:21:59 +0700

    joo commit

---
+++ src/main/java/com/springsource/roo/warehouse/domain/ProductImport.java
@@ -0,0 +1,18 @@
+package com.springsource.roo.warehouse.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class ProductImport {
+
+    /**
+     */
+    @NotNull
+    @Size(min = 5)
+    private String productid;
+}
