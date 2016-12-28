f2990d58545b33c24e06bd03c129debeb58811e4
Kanthika <kan_ti002@hotmail.co.th>
2016-12-28 21:17:52 +0700

    Add field in Entity Tranfer

--- src/main/java/com/springsource/roo/warehouse/domain/Tranfer.java
+++ src/main/java/com/springsource/roo/warehouse/domain/Tranfer.java
@@ -2,6 +2,7 @@ package com.springsource.roo.warehouse.domain;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.ManyToOne;
 
 @RooJavaBean
 @RooToString
@@ -11,4 +12,28 @@ public class Tranfer {
     /**
      */
     private String ordertransfer;
+
+    /**
+     */
+    @ManyToOne
+    private Product product;
+
+    /**
+     */
+    private Integer amount;
+
+    /**
+     */
+    @ManyToOne
+    private Warehouse warehouse;
+
+    /**
+     */
+    @ManyToOne
+    private WarehouseDest warehousedest;
+
+    /**
+     */
+    @ManyToOne
+    private Employee employee;
 }
